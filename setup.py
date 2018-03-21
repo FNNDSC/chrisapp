dd55824743827abe3a0d14d6a5c5c18c0c7ae9bf
Jorge <jbernal0019@yahoo.es>
2018-03-21 10:48:59 -0400

    v1.0.8

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'chrisapp',
-      version          =   '1.0.7',
+      version          =   '1.0.8',
       description      =   '(Python) Base module for Chris plugin apps',
       long_description =   readme(),
       author           =   'FNNDSC',
