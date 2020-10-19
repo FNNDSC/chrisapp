7de4e6a56d6e9b92abc89656f5d2ece5cebd8ca5
Jennings Zhang <jenni_zh@protonmail.com>
2020-10-18 23:36:02 -0400

    Modernize setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,21 @@
-import sys
-
-# Make sure we are running python3.5+
-if 10 * sys.version_info[0]  + sys.version_info[1] < 35:
-    sys.exit("Sorry, only Python 3.5+ is supported.")
-
+from os import path
 from setuptools import setup
 
-def readme():
-    with open('README.rst') as f:
-        return f.read()
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.rst')) as f:
+    readme = f.read()
 
 setup(
-      name             =   'chrisapp',
-      version          =   '1.1.6',
-      description      =   '(Python) Base module for Chris plugin apps',
-      long_description =   readme(),
-      author           =   'FNNDSC',
-      author_email     =   'dev@babymri.org',
-      url              =   'https://github.com/FNNDSC/chrisapp',
-      packages         =   ['chrisapp'],
-      #install_requires =   [],
-      test_suite       =   'nose.collector',
-      tests_require    =   ['nose'],
-      #scripts          =   [],
-      license          =   'MIT',
-      zip_safe=False
+    name             = 'chrisapp',
+    version          = '1.1.7',
+    description      = 'Superclass for Chris plugin apps',
+    long_description = readme,
+    author           = 'FNNDSC',
+    author_email     = 'dev@babymri.org',
+    url              = 'https://github.com/FNNDSC/chrisapp',
+    packages         = ['chrisapp'],
+    test_suite       = 'nose.collector',
+    tests_require    = ['nose'],
+    license          = 'MIT',
+    zip_safe         = False,
+    python_requires='>=3.5'
 )
