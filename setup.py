a6b699bc3a0a8d0f8b6b736d4d6ecdbbc92b0832
Jennings Zhang <jenni_zh@protonmail.com>
2020-10-21 10:33:39 -0400

    Bump version 2.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(path.dirname(path.abspath(__file__)), 'README.rst')) as f:
 
 setup(
     name             = 'chrisapp',
-    version          = '1.1.7',
+    version          = '2.0.0',
     description      = 'Superclass for Chris plugin apps',
     long_description = readme,
     author           = 'FNNDSC',
