40754b9497d29e00b037990f9c038f5f80a1dd1b
Jennings Zhang <jenni_zh@protonmail.com>
2021-02-02 17:31:53 -0500

    2.3.0-1
    Just testing release.yml again

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(path.dirname(path.abspath(__file__)), 'README.rst')) as f:
 
 setup(
     name             = 'chrisapp',
-    version          = '2.3.0',
+    version          = '2.3.0-1',
     description      = 'Superclass for Chris plugin apps',
     long_description = readme,
     author           = 'FNNDSC',
