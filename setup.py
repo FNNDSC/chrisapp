2a77923694aa7d3d5834933ed7cd862e39753cf4
Jennings Zhang <jenni_zh@protonmail.com>
2021-02-09 03:02:31 -0500

    Use importlib_metadata to support old python3.6

--- setup.py
+++ setup.py
@@ -6,16 +6,19 @@ with open(path.join(path.dirname(path.abspath(__file__)), 'README.rst')) as f:
 
 setup(
     name             = 'chrisapp',
-    version          = '2.3.0-1',
+    version          = '2.4.0',
     description      = 'Superclass for Chris plugin apps',
     long_description = readme,
     author           = 'FNNDSC',
     author_email     = 'dev@babymri.org',
     url              = 'https://github.com/FNNDSC/chrisapp',
     packages         = ['chrisapp'],
+    install_requires = [
+        'importlib-metadata; python_version<"3.7"'
+    ],
     test_suite       = 'nose.collector',
     tests_require    = ['nose==1.3.7'],
     license          = 'MIT',
     zip_safe         = False,
-    python_requires = '>=3.7'
+    python_requires = '>=3.6'
 )
